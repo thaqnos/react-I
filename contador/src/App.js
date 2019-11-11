695c36a2c8d5554f789acf89c658a407af5ffa5d
thaqnos <thaqnos@gmail.com>
2019-11-11 13:08:09 -0300

    exemplo de componentes

--- contador/src/App.js
+++ contador/src/App.js
@@ -2,23 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const numero = 0
+
+const iniciaContador = () => {
+  if(numero === 0) {
+    return <p>Contador não iniciado</p>
+  } else {
+    return <p>Contador iniciado</p>
+  }
+}
+
+const Nav = props => {
+  return(
+    <div>
+      <p>{props.nome}</p>
+      <p>{props.titulo}</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav 
+      nome="thai"
+      titulo="Titulommmmmm"
+      />
+      <h1>Contador</h1>
+      <p class="numero">{numero}</p>
+      {iniciaContador()}
+    <div className="buttons">
+      <button className="bton">+1</button>
+      <button className="bton">-1</button>
+    </div>
+    <div>
+      <button className="bton-reset">Resetar</button>
+    </div>
     </div>
   );
 }
