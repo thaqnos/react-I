8b5e4ab664bf8bab4262d18599517e7af8060f11
thaqnos <thaqnos@gmail.com>
2019-11-12 16:28:08 -0300

    adiciona resolução com ternário e toggle

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,45 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css';   
 
-const numero = 0
-
-const iniciaContador = () => {
-  if(numero === 0) {
-    return <p>Contador não iniciado</p>
-  } else {
-    return <p>Contador iniciado</p>
+class Contador extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      contador: 0,
+      mostrar: true
+      // Aqui ficam os estados
+    }
   }
-}
 
-const Nav = props => {
-  return(
-    <div>
-      <p>{props.nome}</p>
-      <p>{props.titulo}</p>
-    </div>
-  )
+  adicionarUm = () => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador + 1
+      }
+    })
+  }
+
+  menosUm = () => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador -1
+      }
+    })
+  }
+
+  resetar = () =>{
+    this.setState(() => {
+      return {
+        contador: 0
+      }
+    })
+  }
+
+  fecharContador = () => {
+    // Aqui eu defino o que acontece com o estado
+    this.setState((prevState) => {
+      console.log(prevState)
+      return {
+        mostrar: !prevState.mostrar
+        // É A MESMA COISA QUE ISSO > mostrar: prevState.mostrar === true ? false : true 
+        // posso fazer uma condição como resposta  
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.contador}</p>
+        <button onClick={this.fecharContador}>
+          {this.state.mostrar === true ? "Bota os botão" : "Tira os botão"}
+          </button><br/>
+        {/* Aqui eu faço a condição sempre usando >THIS< */}
+        {this.state.mostrar === true? '' :(
+          <div>
+            <button className="btnMaisUm" onClick={this.adicionarUm}>+1</button>
+            <button className="btnMenosUm" onClick={this.menosUm}>-1</button>
+            <button className="btnResetar" onClick={this.resetar}>resetar</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <Nav 
-      nome="thai"
-      titulo="Titulommmmmm"
-      />
-      <h1>Contador</h1>
-      <p class="numero">{numero}</p>
-      {iniciaContador()}
-    <div className="buttons">
-      <button className="bton">+1</button>
-      <button className="bton">-1</button>
+      <h2>Contador</h2>
+      <Contador/>
     </div>
-    <div>
-      <button className="bton-reset">Resetar</button>
-    </div>
-    </div>
-  );
+  )
 }
 
 export default App;
